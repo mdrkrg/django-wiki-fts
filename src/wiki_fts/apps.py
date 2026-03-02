6d6548d3f49f614bf84ad5313d89d7a8d809ed40
MidoriKurage <kuragectl@gmail.com>
2026-03-02 13:09:13 +0800

    fix(app): Inject overridden site to wiki.sites.site

--- src/wiki_fts/apps.py
+++ src/wiki_fts/apps.py
@@ -5,3 +5,10 @@ class WikiConfig(AppConfig):
     default_site = "wiki_fts.sites.WikiSite"
     default_auto_field = "django.db.models.AutoField"
     name = "wiki_fts"
+
+    def ready(self):
+        from wiki.sites import site
+        from wiki_fts.sites import WikiSite
+
+        # HACK: Inject site to override
+        site._wrapped = WikiSite()
