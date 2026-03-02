6b10393cdc2ee10ff79760cd3871e04894aa7a84
MidoriKurage <kuragectl@gmail.com>
2026-03-02 09:57:39 +0800

    feat(migrations): Add initial and postgres migrations

---
+++ src/wiki_fts/migrations/0001_initial.py
@@ -0,0 +1,34 @@
+import django.db.models.deletion
+from django.db import migrations, models
+
+import wiki_fts.models
+
+
+class Migration(migrations.Migration):
+    initial = True
+
+    dependencies = [
+        ("wiki", "0003_mptt_upgrade"),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name="SearchIndex",
+            fields=[
+                (
+                    "article",
+                    models.OneToOneField(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        primary_key=True,
+                        related_name="search_index",
+                        serialize=False,
+                        to="wiki.article",
+                    ),
+                ),
+                ("search_vector", wiki_fts.models.SearchVectorField()),
+                ("language", models.CharField(default="simple", max_length=50)),
+                ("created", models.DateTimeField(auto_now_add=True)),
+                ("updated", models.DateTimeField(auto_now=True)),
+            ],
+        ),
+    ]
---
+++ src/wiki_fts/migrations/0002_postgres_gin_index.py
@@ -0,0 +1,45 @@
+from django.db import migrations
+from django.db.migrations.operations.special import SeparateDatabaseAndState
+
+import wiki_fts.models
+
+
+def add_postgres_index(apps, schema_editor):
+    if schema_editor.connection.vendor != "postgresql":
+        return
+    schema_editor.execute(
+        "CREATE INDEX IF NOT EXISTS wiki_fts_gin_index "
+        "ON wiki_fts_searchindex USING GIN (search_vector);"
+    )
+
+
+def remove_postgres_index(apps, schema_editor):
+    if schema_editor.connection.vendor != "postgresql":
+        return
+    schema_editor.execute("DROP INDEX IF EXISTS wiki_fts_gin_index;")
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("wiki_fts", "0001_initial"),
+        ("wiki", "0003_mptt_upgrade"),
+    ]
+
+    operations = [
+        SeparateDatabaseAndState(
+            state_operations=[
+                migrations.AddIndex(
+                    model_name="searchindex",
+                    index=wiki_fts.models.ConditionalGinIndex(
+                        fields=["search_vector"], name="wiki_fts_gin_index"
+                    ),
+                ),
+            ],
+            database_operations=[
+                migrations.RunPython(
+                    add_postgres_index,
+                    remove_postgres_index,
+                ),
+            ],
+        ),
+    ]
